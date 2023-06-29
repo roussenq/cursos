df04b7712508aa3b23b77205a04c59cf8bf757e6
David <davidroussenq@gmail.com>
2023-06-29 11:59:53 -0300

    organizado as pastas

--- devinhouse/modulo01/semana04/projeto_instagram/script.js
+++
@@ -1,7 +0,0 @@
-let email = document.getElementById("iemail").value;
-
-if (email === "") {
-    document.getElementById("iemail").style.border = "1px solid red";
-    document.getElementById("iemail").focus();
-    return false;
-}
